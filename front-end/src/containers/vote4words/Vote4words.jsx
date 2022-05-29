76d581e4cfac058f3f67c69446a14ded9d1b4c00
Erin Rahman <93865608+e-rahman@users.noreply.github.com>
2022-05-29 10:14:42 -0500

    adding features files

--- front-end/src/containers/vote4words/Vote4words.jsx
+++ front-end/src/containers/vote4words/Vote4words.jsx
@@ -3,7 +3,19 @@ import './vote4words.css';
 
 const Vote4Words = () => {
   return (
-    <div>Vote4Words</div>
+    <div className="vote4words__whatvote4words section__margin">
+      <div ClassName="vote4words__whatvote4words-feature">
+        <Feature title="What is Vote4Words" text="placeholder"/>
+      </div>
+      <div ClassName="vote4words__whatvote4words-heading">
+        <h1 ClassName="gradient__text">Tamper-Proof Voting</h1>
+      </div>
+      <div ClassName="vote4words__whatvote4words-container">
+        <Feature title="Placeholder" text="placeholder" />
+        <Feature title="Placeholder" text="placeholder" />
+        <Feature title="Placeholder" text="placeholder" />
+      </div>
+    </div>
   )
 }
 
